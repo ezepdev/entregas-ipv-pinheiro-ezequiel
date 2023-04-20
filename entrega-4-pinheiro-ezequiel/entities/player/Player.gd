9cb128b62c9e52027bfe73d3be3b6463d8099c26
ezepdev <edjpinheiro@gmail.com>
2023-04-19 23:48:33 -0300

    Agrego world y objetos rigidos

--- entrega-4-pinheiro-ezequiel/entities/player/Player.gd
+++ entrega-4-pinheiro-ezequiel/entities/player/Player.gd
@@ -1,21 +1,38 @@
-extends Sprite
+extends KinematicBody2D
 onready var cannon:Sprite = $Cannon
 export var speed = 300;
+export (float) var FRICTION_WEIGHT:float = 0.1;
+export (float) var ACCELERATION:float = 20.0;
+export (float) var HORIZONTAL_SPEED_LIMIT:float = 600;
+export (float) var JUMP_SPEED:float = -500;
+export (float) var GRAVITY:float = 10;
 
 var projectile_container
+var velocity:Vector2 = Vector2.ZERO
 
 func set_projectile_container(container:Node):
 	cannon.projectile_container = container;
 	projectile_container = container;
-	
-	
 
-func _physics_process(delta):
-	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"));
+func _handle_inputs():
+	var horizontal_movement:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"));
+	var player_is_quiet = horizontal_movement == 0 
+	#Cannon rotation
 	var mouse_position:Vector2 = get_global_mouse_position()
 	cannon.look_at(mouse_position)
+	#Cannon fire
+	if(Input.is_action_just_pressed("fire")): cannon.fire()
+	# Player movement	
 	
-	if(Input.is_action_just_pressed("fire")):
-		cannon.fire()
+	if !player_is_quiet:
+		velocity.x = clamp(velocity.x + horizontal_movement * ACCELERATION, -HORIZONTAL_SPEED_LIMIT,HORIZONTAL_SPEED_LIMIT)
+	else:
+		velocity.x = lerp(velocity.x,0,FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
 	
-	position.x += direction_optimized * speed * delta;
+	if (Input.is_action_just_pressed("jump")):
+		velocity.y = JUMP_SPEED
+
+func _physics_process(delta):
+	_handle_inputs();
+	velocity.y += GRAVITY;
+	move_and_slide(velocity,Vector2.UP)
